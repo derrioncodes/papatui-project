cbf56fd61f58695757f02c2ba4df52422e24dd7f
derrioncodes <derrioncodes@gmail.com>
2025-03-23 17:39:49 -0600

    updated mobile nav and mobile subnav

--- paptui-static/js/app.js
+++ paptui-static/js/app.js
@@ -29,16 +29,56 @@ $(document).ready(function () {
 
 // OPEN/CLOSE SHOPPING CART
 $(document).ready(function () {
-  // Open mobile nav when clicking the menu button
+  // Open shopping cart when clicking the menu button
   $(".icon-cart-svg").click(function () {
     $(".shopping-cart__popup__bg").addClass("cart-active");
     $(".shopping-cart__popup").addClass("cart-active");
     
   });
 
-  // Close mobile nav when clicking the close (X) button
+  // Close shopping cart when clicking the close (X) button
   $(".shopping-cart__close-btn").click(function () {
     $(".shopping-cart__popup__bg").removeClass("cart-active");
     $(".shopping-cart__popup").removeClass("cart-active");
+  });
+});
+
+// OPEN/CLOSE MOBILE NAV
+$(document).ready(function () {
+  // Open mobile nav when clicking the menu button
+  $(".mobile__primary-nav").click(function () {
+    $(".mobile__nav__drawer__bg").addClass("active");
+    $(".mobile__nav__drawer__open").addClass("active");
+    
+  });
+
+  // Close mobile nav when clicking the close (X) button
+  $(".mobile__nav__drawer__close-btn").click(function () {
+    $(".mobile__nav__drawer__bg").removeClass("active");
+    $(".mobile__nav__drawer__open").removeClass("active");
+  });
+
+  
+});
+
+
+
+// OPEN/CLOSE MOBILE SUBNAV
+$(document).ready(function () {
+  // Open mobile subnav when clicking a nav item with sublinks
+  $(".mobile__nav__drawer__items__item > .mobile__nav__drawer__link").click(function (e) {
+    e.preventDefault(); // Prevent default link behavior
+    $(this).siblings(".mobile__nav__drawer__subnav-container").addClass("mobile-subnav-active");
+  });
+
+  // Close mobile subnav when clicking the back button
+  $(".mobile__nav__drawer__subnav__back").click(function () {
+    $(this).closest(".mobile__nav__drawer__subnav-container").removeClass("mobile-subnav-active");
+  });
+
+  // Close mobile nav when clicking the close (X) button
+  $(".mobile__nav__drawer__subnav__close-btn").click(function () {
+    $(".mobile__nav__drawer__bg").removeClass("active");
+    $(".mobile__nav__drawer__open").removeClass("active");
   });
 });
